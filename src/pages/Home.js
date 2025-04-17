3c82a3154936ed9f911aa6c5cc59bb08fc856dc0
Tathagat Saha <tathagat.saha@gmail.com>
2025-04-17 16:34:54 +0200

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import Landing from '../components/Landing';
 import NetworkStats from '../components/NetworkStats';
-import AboutSection from '../components/AboutSection';
 import RewardSection from '../components/RewardSection';
-import TeamSection from '../components/TeamSection';
 import ValuesSection from '../components/ValuesSection';
-import VisionSection from '../components/VisionSection';
-import GuidingPrinciples from '../components/GuidingPrinciples';
 
 const Home = () => {
   return (
