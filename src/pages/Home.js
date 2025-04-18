8fc60d502b0135178853f91bbe5a6b5da322d66a
Souvik Basu <bsouvik986.com>
2025-04-18 15:25:55 +0530

    Last Home.js removed_Guiding_principal component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
       {/* <TeamSection /> */} 
       <ValuesSection />
       <VisionSection />
-      <GuidingPrinciples />
+      {/* <GuidingPrinciples /> */}
     </div>
   );
 };
